a39b2823d96d37af0366bedc98d819774e343994
Thiago Bonfim <bonfimtm@gmail.com>
2022-01-31 15:14:47 +0100

    Mover JavaScript para arquivo separado

---
+++ src/main.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAyTLTqMuDSaf4DnrPK-pYJe4f29h0g-lk",
+  authDomain: "telegrama-91b14.firebaseapp.com",
+  projectId: "telegrama-91b14",
+});
+
+var db = firebase.firestore();
+
+db.collection("mensagens")
+  .orderBy("criacao", "desc")
+  .onSnapshot((querySnapshot) => {
+    const mensagensElm = document.querySelector("#mensagens");
+    mensagensElm.replaceChildren();
+    querySnapshot.forEach((doc) => {
+      const pElm = document.createElement("p");
+      const textElm = document.createTextNode(doc.data().conteudo);
+      pElm.appendChild(textElm);
+      mensagensElm.appendChild(pElm);
+    });
+  });
+
+function enviarMensagem(event) {
+  event.preventDefault();
+  let conteudo = document.querySelector("#conteudo").value;
+  let criacao = new Date();
+  if (conteudo) {
+    db.collection("mensagens")
+      .add({ conteudo, criacao })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      })
+      .finally(() => {
+        document.querySelector("#conteudo").value = "";
+      });
+  }
+}
